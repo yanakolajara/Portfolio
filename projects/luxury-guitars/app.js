4107d6cba047a63c7187bfbdb511b21695b6e673
Yanako Lajara <yanakolajara@pursuit.org>
2023-05-25 23:20:04 -0400

    random-user-generator finished

--- projects/luxury-guitars/app.js
+++ projects/luxury-guitars/app.js
@@ -2,56 +2,74 @@ const form = document.querySelector(".newItem")
 let refreshRemoveListener = false;
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const name = document.querySelector("#guitarName").value
-    let price = document.querySelector("#price").value
-    const stock = document.querySelector("#stock").value
-    const weight = document.querySelector("#weight").value
-    const length = document.querySelector("#length").value
-    const imageLink = document.querySelector("#imageLink").value
+    try{
+      if(document.querySelector("#guitarName").value.length < 4){
+        throw new Error("Guitar name should be at least 4 characters")
+      }
+      
+      const name = document.querySelector("#guitarName").value
+      let price = document.querySelector("#price").value
+      const stock = document.querySelector("#stock").value
+      const weight = document.querySelector("#weight").value
+      const length = document.querySelector("#length").value
+      const imageLink = document.querySelector("#imageLink").value
+  
+      let newDiv = document.createElement("div")
+      newDiv.setAttribute("class", "guitar")
+      const main = document.getElementById("guitars")
+      main.appendChild(newDiv)
+  
+      let newImage = document.createElement("img")
+      newImage.setAttribute("src", imageLink)
+      newImage.setAttribute("alt", "guitar image")
+      newDiv.appendChild(newImage)
+  
+      let newName = document.createElement("p")
+      newName.appendChild(document.createTextNode(name))
+      newDiv.appendChild(newName)
+  
+      let newPrice = document.createElement("p")
+      price = price.split('')
+      price.unshift('$')
+      price.splice(-3,0,',')
+      newPrice.appendChild(document.createTextNode(price.join('')))
+      newDiv.appendChild(newPrice)
+  
+      let newStock = document.createElement("p")
+      newStock.setAttribute("class", "stock")
+      let stockValue = NaN
+      stock === "yes" ? stockValue = "(in stock)" : stockValue = "(out of stock)"
+      const stockText = document.createTextNode(stockValue)
+      newStock.appendChild(stockText)
+      newDiv.appendChild(newStock)
+  
+      let newButton = document.createElement("button")
+      newButton.setAttribute("class", "remove")
+      newButton.appendChild(document.createTextNode("Remove"))
+      newDiv.appendChild(newButton)
+  
+      let newWeight = document.createElement("p")
+      newWeight.setAttribute("class", "moreInfo")
+      newWeight.appendChild(document.createTextNode(`weight: ${weight} lb`))
+      newDiv.appendChild(newWeight)
+  
+      let newLength = document.createElement("p")
+      newLength.setAttribute("class", "moreInfo")
+      newLength.appendChild(document.createTextNode(`length: ${length}"`))
+      newDiv.appendChild(newLength)
 
-    let newDiv = document.createElement("div")
-    newDiv.setAttribute("class", "guitar")
-    const main = document.getElementById("guitars")
-    main.appendChild(newDiv)
-
-    let newImage = document.createElement("img")
-    newImage.setAttribute("src", imageLink)
-    newImage.setAttribute("alt", "guitar image")
-    newDiv.appendChild(newImage)
-
-    let newName = document.createElement("p")
-    newName.appendChild(document.createTextNode(name))
-    newDiv.appendChild(newName)
-
-    let newPrice = document.createElement("p")
-    price = price.split('')
-    price.unshift('$')
-    price.splice(-3,0,',')
-    newPrice.appendChild(document.createTextNode(price.join('')))
-    newDiv.appendChild(newPrice)
-
-    let newStock = document.createElement("p")
-    newStock.setAttribute("class", "stock")
-    let stockValue = NaN
-    stock === "yes" ? stockValue = "(in stock)" : stockValue = "(out of stock)"
-    const stockText = document.createTextNode(stockValue)
-    newStock.appendChild(stockText)
-    newDiv.appendChild(newStock)
-
-    let newButton = document.createElement("button")
-    newButton.setAttribute("class", "remove")
-    newButton.appendChild(document.createTextNode("Remove"))
-    newDiv.appendChild(newButton)
-
-    let newWeight = document.createElement("p")
-    newWeight.setAttribute("class", "moreInfo")
-    newWeight.appendChild(document.createTextNode(`weight: ${weight} lb`))
-    newDiv.appendChild(newWeight)
-
-    let newLength = document.createElement("p")
-    newLength.setAttribute("class", "moreInfo")
-    newLength.appendChild(document.createTextNode(`length: ${length}"`))
-    newDiv.appendChild(newLength)
+      const errorDelete = document.getElementById("error-display")
+      if(errorDelete){
+        document.querySelector("#error-display").remove()
+      } 
+    }
+    catch(error){
+      console.log(error)
+      let errorMessage = document.createElement("p")
+      errorMessage.appendChild(document.createTextNode(error))
+      errorMessage.setAttribute("id", "error-display")
+      document.querySelector(".errorMessage").append(errorMessage)
+    }
 })
 
 form.addEventListener("reset", (element) => {
@@ -80,3 +98,6 @@ main.addEventListener('click', (event) => {
 const mainPage = () => {
   window.location.href = ".../main.html"
 }
+
+
+
--- projects/random-user-generator/app.js
+++ projects/random-user-generator/app.js
@@ -1,16 +1,57 @@
 fetch("https://randomuser.me/api")
 .then((response) => response.json())
 .then((json) => {
-    console.log(json)
+    const list = json.results[0]
+    const name = document.createElement("p")
+    name.appendChild(document.createTextNode(`${list.name.title} ${list.name.first}${list.name.last}`))
+    document.querySelector(".divName").append(name)
+    
+    const image = document.createElement("img")
+    image.setAttribute("class", "image")
+    image.setAttribute("src", list.picture.large)
+    document.querySelector(".divImage").append(image)
+
+    const username = document.createElement("p")
+    username.appendChild(document.createTextNode(`Username: ${list.login.username}`))
+    document.querySelector(".username").append(username)
+
+    let date = (list.dob.date).substring(0, 10)
+    date = date.split('-')
+    date = [date[1],date[2],date[0]]
+    date = date.join('/')
+    const dob = document.createElement("p")
+    dob.appendChild(document.createTextNode(`Date of birth: ${date} (age: ${list.dob.age})`))
+    document.querySelector(".dob").append(dob)
+
+    const street = document.createElement("p")
+    street.appendChild(document.createTextNode(`${list.location.street.number} ${list.location.street.name}, ${list.location.city}`))
+    document.querySelector(".address").append(street)
+
+    const state = document.createElement("p")
+    state.appendChild(document.createTextNode(`${list.location.state}, ${list.location.postcode}`))
+    document.querySelector(".address").append(state)
+
+    const country = document.createElement("p")
+    country.appendChild(document.createTextNode(`${list.location.country}`))
+    document.querySelector(".address").append(country)
+
+    const email = document.createElement("p")
+    email.appendChild(document.createTextNode(`email: ${list.email}`))
+    email.setAttribute("class", "email")
+    document.querySelector("footer").append(email)
+
+    const phone = document.createElement("p")
+    phone.appendChild(document.createTextNode(`phone: ${list.cell}`))
+    phone.setAttribute("class", "phone")
+    document.querySelector("footer").append(phone)
+
+    const reload = document.createElement("input")
+    reload.setAttribute("onclick", "location.reload()")
+    reload.setAttribute("type", "button")
+    reload.setAttribute("value", "New user")
+    document.querySelector(".address").append(reload)
+
 })
 .catch((error) => {
     console.log(error)
-})
-
-const form = document.querySelector(".formTest")
-console.log(form)
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    console.log("a")
 })
